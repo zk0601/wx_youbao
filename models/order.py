65c4613d31b7a004a876d9a83da653a303148c66
zhoukun <m15927260404@163.com>
2018-11-28 15:58:41 +0800

    1

--- models/order.py
+++ models/order.py
@@ -10,13 +10,13 @@ class Order(Base):
     __tablename__ = 'order'
 
     id = Column(INTEGER, primary_key=True, autoincrement=True)
-    product_id = Column(INTEGER, ForeignKey('product.id'), nullable=False)
+    product_id = Column(INTEGER, nullable=False)
     product_name = Column(VARCHAR(255), nullable=False)
     product_description = Column(VARCHAR(255), nullable=False)
     total_fee = Column(DECIMAL(8, 2), nullable=False)
     create_ts = Column(DATETIME, default=datetime.now(), nullable=False)
     pay_status = Column(INTEGER, default=None)
-    user_openid = Column(VARCHAR(255), ForeignKey('user_base.openid'), nullable=False)
+    user_openid = Column(VARCHAR(255), nullable=False)
     complete_time = Column(DATETIME)
     out_trade_no = Column(VARCHAR(255), nullable=False)
 
