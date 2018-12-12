eb3d34f4b1bebddf1a3d9c9b5c23ed80d38b547d
zhoukun <m15927260404@163.com>
2018-12-12 16:57:04 +0800

    1

--- config/setting.py
+++ config/setting.py
@@ -4,10 +4,14 @@ from tornado.options import define
 define("port", default=8888, help="run on the given port", type=int)
 
 #mysql test
-define("mysql_host", default="47.91.252.155:3306", help="database host")
+# define("mysql_host", default="47.91.252.155:3306", help="database host")
+# define("mysql_database", default="youbao", help="database name")
+# define("mysql_user", default="root", help="database user")
+# define("mysql_password", default="123456", help="database password")
+define("mysql_host", default="47.75.49.133:3306", help="database host")
 define("mysql_database", default="youbao", help="database name")
 define("mysql_user", default="root", help="database user")
-define("mysql_password", default="123456", help="database password")
+define("mysql_password", default="4608310zk", help="database password")
 
 define("pool_size", default=20, help="pool size")
 define("pool_recycle", default=3600, help="pool recycle")
@@ -19,11 +23,15 @@ define("max_workers", default=25, type=int, help="max threads")
 define("secret", default='Youbao~348#fgMFHz24$9deHPfL', help="token secret")
 
 #wechat config
-define("AppID", default='wxdd1aae652aa93507', help='wechat develop id')
-define('APPSecret', default='cd2cd98d1c3c34f4093a1ade46fe342d', help='wechat secret')
+# define("AppID", default='wxdd1aae652aa93507', help='wechat develop id')
+# define('APPSecret', default='cd2cd98d1c3c34f4093a1ade46fe342d', help='wechat secret')
+define("AppID", default='wx39e54711a30945e2', help='wechat develop id')
+define('APPSecret', default='950cefbcc48d62ebfdbae6639f4486b4', help='wechat secret')
 
-define("wx_token", default='Youbao123456', help='wechat token')
-define("EncodingAESKey", default='ysV2I4oa7s9hC60TMA5TTm0Na4hk8fhLZ7krvrlXZ5d', help='wechat EncodingAESKey')
+# define("wx_token", default='Youbao123456', help='wechat token')
+# define("EncodingAESKey", default='ysV2I4oa7s9hC60TMA5TTm0Na4hk8fhLZ7krvrlXZ5d', help='wechat EncodingAESKey')
+define("wx_token", default='Jinlibao230943', help='wechat token')
+define("EncodingAESKey", default='URIkgXtyJ1oNJL2aSYfn7L16n0HoWDE8P37wou69O2Z', help='wechat EncodingAESKey')
 
 # payment
 define('merchant_id', default='1520796591', help='wechat merchant number')
