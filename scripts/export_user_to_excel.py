4ad3a17bf06886f5f865c8c5361f0283164ead69
zhoukun <m15927260404@163.com>
2019-01-10 17:34:02 +0800

    update email

--- scripts/export_user_to_excel.py
+++ scripts/export_user_to_excel.py
@@ -8,29 +8,46 @@ import os
 import datetime
 
 from scripts.crontab_to_notify_event import TemplateNotify
-from models.user import Phone_Name, User_Base, User_From
+from models.user import Phone_Name, User_Base, User_From, Spouse, Children
 from models.order import Order
 
 
 class Export(TemplateNotify):
     def __init__(self):
         super(Export, self).__init__()
+        self.family_str = {
+            '1': '单身',
+            '2': '已婚无娃',
+            '3': '离婚有娃',
+            '4': '已婚有娃'
+        }
+        self.other_str = {
+            '0': '否',
+            '1': '是'
+        }
 
     def main(self):
         try:
-            # headers = ['用户微信昵称', '用户微信头像', '是否填写姓名电话', '姓名', '电话', '微信' ,'是否填写详细信息', '订单支付时间']
-            headers = ['nick_name', 'image', 'is_phone', 'name', 'phone', 'wechat', 'is_detail', 'order_time']
+            # headers = ['用户微信昵称', '用户微信头像', '姓名', '电话', '微信', '订单支付时间', '性别', '家庭情况',
+            #            '孩子数量', '是否赡养父母', '出生日期', '疾病', '年收入（万元）', '职业', '是否有社保',
+            #            '房贷总额（万元）', '房贷月供（元）', '还需还款年数', '车贷总额（万元）', '车贷月供（元）',
+            #            '还需还款年数', '是否经常出差', '日常出行是否驾车', '所在城市',
+            #            '配偶生日', '配偶疾病', '配偶收入（万元）', '配偶职业', '配偶是否有社保', '配合是否经常出差',
+            #            '配偶日常出行是否驾车', '第一个孩子性别', '第一个孩子生日', '第一个孩子疾病',
+            #            '第二个孩子性别', '第二个孩子生日', '第二个孩子疾病',  '第三个孩子性别', '第三个孩子生日', '第三个孩子疾病',
+            #            ]
+            headers = ['nick_name', 'image', 'name', 'phone', 'wechat', 'order_time', 'gender', 'family',
+                       'children_num', 'is_supportparents', 'birthday', 'disease', 'income', 'profession',
+                       'has_socialsecurity', 'houseloans_total', 'houseloans_permonth', 'houseloans_years',
+                       'carloans_total', 'carloans_permonth', 'carloans_years', 'offen_businesstravel', 'offen_car',
+                       'city', 'spouse_birthday', 'spouse_disease', 'spouse_income', 'spouse_profession', 'spouse_security',
+                       'spouse_businesstravel', 'spouse_car', 'first_child_gender', 'first_child_birthday',
+                       'first_child_disease', 'second_child_gender', 'second_child_birthday', 'second_child_disease',
+                       'third_child_gender', 'third_child_birthday', 'third_child_disease']
             csv_list = []
-            data_template = {
-                'nick_name': '',
-                'image': '',
-                'is_phone': '',
-                'name': '',
-                'phone': '',
-                'wechat': '',
-                'is_detail': '',
-                'order_time': ''
-            }
+            data_template = dict()
+            for item in headers:
+                data_template[item] = ''
             users = self.session.query(Order).filter(Order.pay_status == 2).all()
             for user in users:
                 data = data_template.copy()
@@ -40,18 +57,56 @@ class Export(TemplateNotify):
                 data['nick_name'] = base.nickname
                 data['image'] = base.image_url
                 phone = self.session.query(Phone_Name).filter(Phone_Name.openid == openid).first()
-                if not phone:
-                    data['is_phone'] = '否'
-                    data['name'], data['phone'], data['wechat'] = '', '', ''
-                else:
-                    data['is_phone'] = '是'
+                if phone:
                     data['name'], data['phone'], data['wechat'] = phone.name, str(phone.phone), phone.wx_number
                 form_data = self.session.query(User_From).filter(User_From.openid == openid).first()
-                if not form_data:
-                    data['is_detail'] = '否'
-                else:
-                    data['is_detail'] = '是'
                 data['order_time'] = user.complete_time.strftime("%Y-%m-%d %H:%M:%S")
+                if form_data:
+                    data['gender'] = form_data.gender
+                    data['family'] = self.family_str[str(form_data.family)]
+                    data['children_num'] = form_data.children_num
+                    data['is_supportparents'] = self.other_str[str(form_data.is_supportparents)]
+                    data['birthday'] = form_data.birthday
+                    data['disease'] = form_data.disease
+                    data['income'] = form_data.income
+                    data['profession'] = form_data.profession
+                    data['has_socialsecurity'] = self.other_str[str(form_data.has_socialsecurity)]
+                    data['houseloans_total'] = form_data.houseloans_total
+                    data['houseloans_permonth'] = form_data.houseloans_permonth
+                    data['houseloans_years'] = form_data.houseloans_years
+                    data['carloans_total'] = form_data.carloans_total
+                    data['carloans_permonth'] = form_data.carloans_permonth
+                    data['carloans_years'] = form_data.carloans_years
+                    data['offen_businesstravel'] = self.other_str[str(form_data.offen_businesstravel)]
+                    data['offen_car'] = self.other_str[str(form_data.offen_car)]
+                    data['city'] = form_data.city
+
+                    spouse = self.session.query(Spouse).filter(Spouse.id == form_data.spouse_id).first()
+                    if spouse.id != 0:
+                        data['spouse_birthday'] = spouse.birthday
+                        data['spouse_disease'] = spouse.disease
+                        data['spouse_income'] = spouse.income
+                        data['spouse_profession'] = spouse.profession
+                        data['spouse_security'] = self.other_str[str(spouse.has_socialsecurity)]
+                        data['spouse_businesstravel'] = self.other_str[str(spouse.offen_businesstravel)]
+                        data['spouse_car'] = self.other_str[str(spouse.offen_car)]
+
+                    first_child = self.session.query(Children).filter(Children.id == form_data.first_child_id).first()
+                    if first_child.id != 0:
+                        data['first_child_gender'] = first_child.gender
+                        data['first_child_birthday'] = first_child.birthday
+                        data['first_child_disease'] = first_child.disease
+                    second_child = self.session.query(Children).filter(Children.id == form_data.second_child_id).first()
+                    if second_child.id != 0:
+                        data['second_child_gender'] = second_child.gender
+                        data['second_child_birthday'] = second_child.birthday
+                        data['second_child_gender'] = second_child.disease
+                    third_child = self.session.query(Children).filter(Children.id == form_data.third_child_id).first()
+                    if third_child.id != 0:
+                        data['third_child_gender'] = third_child.gender
+                        data['third_child_birthday'] = third_child.birthday
+                        data['third_child_gender'] = third_child.disease
+
                 csv_list.append(data)
 
             today = datetime.datetime.now().strftime("%Y%m%d")
--- scripts/send_email.py
+++ scripts/send_email.py
@@ -16,8 +16,8 @@ def send_email(text, header):
             send_file = os.path.join(os.path.dirname(__file__), "user_export", "%s.csv" % today)
             msg = MIMEMultipart()
             msg['From'] = _format_addr('锦鲤好运保<%s>' % '15927260404@139.com')
-            msg['To'] = '934170914@qq.com,635925409@qq.com'
-            # msg['To'] = '515350159@qq.com,15927260404@139.com'
+            msg['To'] = '934170914@qq.com'
+            # msg['To'] = '15927260404@139.com'
             msg['Subject'] = Header(header, 'utf-8').encode()
             msg.attach(MIMEText(text, 'plain', 'utf-8'))
 
@@ -28,8 +28,8 @@ def send_email(text, header):
             server = smtplib.SMTP_SSL('smtp.139.com', 465)
             server.set_debuglevel(1)
             server.login('15927260404@139.com', '4608310zk')
-            server.sendmail('15927260404@139.com', '934170914@qq.com, 635925409@qq.com'.split(','), msg.as_string())
-            # server.sendmail('15927260404@139.com', '515350159@qq.com, 15927260404@139.com'.split(','), msg.as_string())
+            server.sendmail('15927260404@139.com', '934170914@qq.com'.split(','), msg.as_string())
+            # server.sendmail('15927260404@139.com', '15927260404@139.com'.split(','), msg.as_string())
             break
 
         except Exception as e:
@@ -45,8 +45,48 @@ def _format_addr(s):
 if __name__ == '__main__':
     text = """
         此名单为所有用户购买名单，不区分日期。
-        \n    每列代表的中文含义从左至右为 “用户微信昵称, 用户微信头像, 是否填写姓名电话, 姓名, 电话, 是否填写详细信息”
         \n    可用excel筛选功能自主选择信息
+        \n    列名解释：
+        \n              nick_name : 用户微信昵称\t
+                        image : 用户微信头像\t
+                        name : 姓名\t
+                        phone : 电话\t
+                        wechat : 微信\t
+                        order_time : 订单支付时间\t
+                        gender : 性别\t
+                        family : 家庭情况\t
+                        children_num : 孩子数量\t
+                        is_supportparents : 是否赡养父母\t
+                        birthday : 出生日期\t
+                        disease : 疾病\t
+                        income : 年收入（万元）\t
+                        profession : 职业\t
+                        has_socialsecurity : 是否有社保\t
+                        houseloans_total : 房贷总额（万元）\t
+                        houseloans_permonth : 房贷月供（元）\t
+                        houseloans_years : 还需还款年数\t
+                        carloans_total : 车贷总额（万元）\t
+                        carloans_permonth : 车贷月供（元）\t
+                        carloans_years : 还需还款年数\t
+                        offen_businesstravel : 是否经常出差\t
+                        offen_car : 日常出行是否驾车\t
+                        city : 所在城市\t
+                        spouse_birthday : 配偶生日\t
+                        spouse_disease : 配偶疾病\t
+                        spouse_income : 配偶收入（万元）\t
+                        spouse_profession : 配偶职业\t
+                        spouse_security : 配偶是否有社保\t
+                        spouse_businesstravel : 配合是否经常出差\t
+                        spouse_car : 配偶日常出行是否驾车\t
+                        first_child_gender : 第一个孩子性别\t
+                        first_child_birthday : 第一个孩子生日\t
+                        first_child_disease : 第一个孩子疾病\t
+                        second_child_gender : 第二个孩子性别\t
+                        second_child_birthday : 第二个孩子生日\t
+                        second_child_disease : 第二个孩子疾病\t
+                        third_child_gender : 第三个孩子性别\t
+                        third_child_birthday : 第三个孩子生日\t
+                        third_child_disease : 第三个孩子疾病\t
         \n
         \n备注：附件文件为.csv格式，采用utf-8编码，若使用office的excel打开出现乱码，先将文件编码改为ANSI即可
     """
